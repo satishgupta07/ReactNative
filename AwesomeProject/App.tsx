aa2d8e911865f5cce10a4afd32af95f5c42677df
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-10-22 17:27:47 +0530

    First App | Styling React Native Components
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native'
+
+function App(){
+  return(
+    <SafeAreaView>
+    <View>
+      <Text>Hello World !</Text>
+    </View>
+  </SafeAreaView>
+  )
+}
+
+export default App;
