98097e3861d2e26f4aa3f1094ccfe6b328889ffd
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-10-31 19:01:02 +0530

    Iteration
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- Styler_02/App.tsx
+++ Styler_02/App.tsx
@@ -4,6 +4,7 @@ import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCard from './components/FancyCard'
 import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <ElevatedCards />
         <FancyCard />
         <ActionCard />
+        <ContactList />
       </ScrollView>
     </SafeAreaView>
   )
